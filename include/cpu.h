2e0c625366e34be71b659a343b900772ed8e883d
Harry <107435091+hazzaaclark@users.noreply.github.com>
2023-04-26 21:13:27 +0100

    Update cpu.h

--- include/cpu.h
+++ include/cpu.h
@@ -5,12 +5,15 @@
 /* THIS FILE PERTAINS TO THE DECLARATION OF DEFINES FOR THE CPU */
 /* ALL OF THESE DEFINES ARE MEANT TO REPRESENT THE CPU ON A LOW LEVEL */
 
+/* DOCUMENTATION USED: https://www.nxp.com/docs/en/reference-manual/MC68000UM.pdf */
+
 #ifndef CPU_DEFINES
 #define CPU_DEFINES
 
 /* NESTED INCLUDES */
 
 #include "common.h"
+#include "sys_calls.h"
 
 #define CPU_TLB U64
 
@@ -27,5 +30,13 @@ static CPU_TARGET_LONG_SIZE(CPU_TLB / 8);
 
 #define ADDRESS U64
 #define ADDRESS_BITS 64
+
+typedef struct CONDITIONS
+{
+	typedef U32* DATA_REGISTER[8];
+	typedef U32* ADDRESS_REGISTER[8];
+	typedef U32* PROGRAM_COUNTER;
+	typedef U32* STATUS_REGISTER;
+};
 
 #endif
